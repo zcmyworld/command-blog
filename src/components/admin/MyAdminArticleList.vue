8b24bdd40974aa718c66a1205fc5e93601353945
zcmyworld <zhicong@zcmyworld.com>
2017-08-31 18:33:35 +0800

    增加組件

---
+++ src/components/admin/MyAdminArticleList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="admin-article-list">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MyAdminArticleList',
+}
+</script>
+
+<style scoped>
+  .admin-article-list {
+    border: 1px solid rgb(180, 180, 180);
+    width: 950px;
+    min-height: 500px;
+    margin-left: 30px;
+    background-color: white;
+    border-radius: 5px;
+    padding: 20px;
+    box-sizing: border-box;
+  }
+</style>
--- src/components/admin/MyAdminPanel.vue
+++ src/components/admin/MyAdminPanel.vue
@@ -1,24 +1,21 @@
 <template>
-  <div class="my-admin-panel">
-    panel
+  <div class="my-admin-panel-box">
+    <my-admin-article-list></my-admin-article-list>
   </div>
 </template>
 
 <script>
+import MyAdminEditArticle from '@/components/admin/MyAdminEditArticle.vue';
+import MyAdminArticleList from '@/components/admin/MyAdminArticleList.vue';
 export default {
   name: 'MyAdminPanel',
+  components: {
+    MyAdminEditArticle,
+    MyAdminArticleList
+  }
 }
 </script>
 
 <style scoped>
-  .my-admin-panel {
-    border: 1px solid rgb(180, 180, 180);
-    width: 950px;
-    height: 500px;
-    margin-left: 30px;
-    background-color:white;
-    border-radius: 5px;
-    padding: 20px;
-    box-sizing: border-box;
-  } 
+  
 </style>
