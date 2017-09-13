5fb3884c2047d72a7e027842edf376deb39d7558
zcmyworld <zhicong@zcmyworld.com>
2017-09-13 14:56:53 +0800

    完成一次前後端交互

--- src/plugins/article/MyContent.vue
+++ src/plugins/article/MyContent.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="content">
-    <div class="article">
-      <router-link to="/article/1">
-        <h1 class="title">Vue腳手架搭建</h1>
-        <span>Posted on 2017.05.02</span>
-        <p>构建一个Vue工程最快捷的方法是莫过于使用Vue官方提供的命令行工具刷新，对代码进行压缩打包，支持ES6语法等等，实现这些功能使用了大量的npm模块和webpack插件，这对想要了解整个工程架构的初学者而言并不友好。本系列文章将从最基础的功能开始构建，逐步完成一个完整的工程项目（最终大概会和vue-cli生成的项目长的一样）。</p>
-      </router-link>
-    </div>
-    <div class="article">
-      <router-link to="/article/1">
-        <h1 class="title">Vue腳手架搭建</h1>
-        <span>Posted on 2017.05.02</span>
-        <p>构建一个Vue工程最快捷的方法是莫过于使用Vue官方提供的命令行工具刷新，对代码进行压缩打包，支持ES6语法等等，实现这些功能使用了大量的npm模块和webpack插件，这对想要了解整个工程架构的初学者而言并不友好。本系列文章将从最基础的功能开始构建，逐步完成一个完整的工程项目（最终大概会和vue-cli生成的项目长的一样）。</p>
-      </router-link>
+    <div v-for="article in articles">
+      <div class="article">
+        <router-link to="/article/1">
+          <h1 class="title">{{article.title}}</h1>
+          <span>Posted on {{article.posted}}</span>
+          <p>{{article.summary}}</p>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -24,9 +19,15 @@ export default {
   components: {
     MyArticle
   },
+  data: function() {
+    return {
+      articles: []
+    }
+  },
   created: function() {
     this.$http.get('http://itos.dev.com/articles').then((res) =>{
       this.articles = res.body;
+      return this;
     })
   }
 
