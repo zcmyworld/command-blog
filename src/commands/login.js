4853fc36b17d3333cfb97973d15d40e9a76b3459
zcmyworld <zhicong@zcmyworld.com>
2017-09-21 18:29:07 +0800

    backup

--- src/commands/login.js
+++ src/commands/login.js
@@ -1,7 +1,31 @@
 import ITOS from '@/ITOS';
+import $ from 'jquery';
 
 export default {
   name: 'login',
   exec(command) {
+    // console.log(ITOS.Terminal.getCommandArgs());
+    var args = ITOS.Terminal.getCommandArgs();
+    if (args.length != 2) {
+      // ITOS.Terminal.print('Command not implemented for that parameter');
+      // return;
+    }
+    // var uname = args[0];
+    // var pwd = args[1];
+    $.ajax({
+      method: "post",
+      url: `http://itos.dev.com/user/login`,
+      data: {
+        uname: 'itgo',
+        pwd: '1234qwer'
+      },
+      async: false
+    }).done((msg, status, xhr) => {
+      console.log(xhr)
+      console.log(xhr.getAllResponseHeaders())
+      var cookie = xhr.getResponseHeader('Set-Cookie');
+      console.log(cookie)
+      console.log(msg)
+    });
   }
 }
--- src/plugins/article/commands/edit.js
+++ src/plugins/article/commands/edit.js
@@ -1,6 +1,6 @@
 import ITOS from '@/ITOS';
 import { EventBus } from '../event-bus.js';
-
+import $ from 'jquery';
 
 export default {
   exec(route_path) {
@@ -8,7 +8,17 @@ export default {
     var currentRoute = ITOS.Router.router.currentRoute.path;
     //判斷路由, /article/:id
     if (/^\/article\/[0-9]*$/.test(currentRoute)) {
-      EventBus.$emit('content_edit');
+      $.ajax({
+        method: "get",
+        url: `http://itos.dev.com/user/islogin`,
+        async: false
+      }).done((msg) => {
+        if (msg.data.isLogin) {
+          EventBus.$emit('content_edit');
+        } else {
+          ITOS.Terminal.print('Permission defined');
+        }
+      });
     } else {
       ITOS.Terminal.print('You need to open and article to use this function');
     }
