ea1010b8694d96ab6783abe4339e9743aac47b7f
zcmyworld <zhicong@zcmyworld.com>
2017-10-12 11:49:22 +0800

    退出登录的前端交互

--- src/commands/bye.js
+++ src/commands/bye.js
@@ -1,7 +1,24 @@
 import ITOS from '@/ITOS';
 
+import $ from 'jquery';
+
 export default {
   name: 'bye',
   exec(command) {
+    $.ajax({
+      method: "get",
+      headers: {
+        'sessionkey': ITOS.Session.getSessionKey()
+      },
+      url: `http://itos.dev.com/user/logout`,
+      async: false
+    }).done((resData, status, xhr) => {
+      if (resData.error != 0) {
+        ITOS.Terminal.print(resData.msg)
+        return;
+      }
+      ITOS.Session.saveSessionKey(resData.data.sessionkey);
+      ITOS.Terminal.print(resData.msg);
+    });
   }
 }
